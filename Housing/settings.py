09c9a0517906129727708a67c08dd97cc50c86b5
Shiva Manandhar <sm4phs@virginia.edu>
2021-09-26 12:38:51 -0400

    requirements

--- Housing/settings.py
+++ Housing/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(file))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
